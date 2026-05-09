f8b8e96d04d185ceac52ce1c356d642b8d7eb4a8
Iham93 <aditjota@gmail.com>
2026-05-10 01:55:23 +0700

    fix: nuclear cors and router flags

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,16 +1,11 @@
 <?php
-
 namespace App\Http\Middleware;
-
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class Cors
-{
-    public function handle(Request $request, Closure $next): Response
-    {
-        // Tangani Preflight (OPTIONS) secara eksplisit
+class Cors {
+    public function handle(Request $request, Closure $next): Response {
         if ($request->isMethod('OPTIONS')) {
             return response('', 204)
                 ->header('Access-Control-Allow-Origin', '*')
@@ -18,10 +13,7 @@ class Cors
                 ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization, Accept, Origin, X-Token-Auth')
                 ->header('Access-Control-Allow-Credentials', 'true');
         }
-
         $response = $next($request);
-
-        // Tambahkan header ke semua response (Success maupun Error)
         return $response
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH')
